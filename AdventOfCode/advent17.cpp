01af4b6b0411467c9f937cd875225a2004f1a204
McJazzy <michieljaspers90@gmail.com>
2017-02-05 12:50:31 +0100

    day17 issues with vcc..

---
+++ AdventOfCode/advent17.cpp
@@ -0,0 +1,57 @@
+#include "advent.h"
+#include "md5.h"
+#include <list>
+#include <array>
+#include <string>
+
+struct pos {
+	int x; int y;
+	pos operator+(const pos & other) { return{ x + other.x, y + other.y }; }
+	bool operator==(const pos & other) { return  x == other.x && y == other.y; }
+};
+struct state { std::string path;  pos p;};
+
+template <>
+std::string process<17>(std::istream & is, bool part2) {
+	std::list<state> s, found;		
+	std::string input;
+	is >> input;
+	const pos orig{ 0,0 }, end{ 3,3 };
+	state orig_s;
+	orig_s.path = ""; orig_s.p = orig;
+	std::array<state, 4> dirs; 
+	dirs[0] =  { "U", {0, 1}};
+	dirs[1] = { "D", { 0, -1 }};
+	dirs[2] = { "L",{ -1, 0 }};
+	dirs[3] = { "R", { 1, 0} };
+	//std::vector<state> dirs; dirs.push_back({ "U",{{ 0,1 }} }); dirs.push_back({ "D",{{ 0,-1 }} }); dirs.push_back({ "L",{{ -1,0 }} }); dirs.push_back({ "R",{{ 1,0 }} });
+
+	for (s.push_back(orig_s); s.size() != 0 && !found.size(); s.pop_front()) {
+		auto & curr = s.front();
+		std::copy(curr.path.begin(), curr.path.end(), input.end());
+		std::string md5_hash = MD5(input).hexdigest();
+		for (int i = 0; i < 4; ++i) {			
+			if (md5_hash[i] > 'a') {
+				//state n{ curr.path + dirs[i].path ,curr.p + dirs[i].p};
+			//	bool in = n.p.x > orig.x && n.p.x < end.x && n.p.y > orig.y && n.p.y < end.y;
+			//	if (n.p == end)
+					//found.push_back(n);				
+				//else if (in)
+					//s.push_back(n);
+			}
+		}
+	}
+	return found.back().path;	
+}
+
+template <> 
+void solve<17>() {
+	std::cout << "advent17: " << process<17>(std::stringstream("hhhxzeay"), false) << std::endl;
+}
+
+template <>
+void test<17>() {
+	assert(process<17>(std::stringstream("ihgpwlah"), false) == "DDRRRD");
+	assert(process<17>(std::stringstream("kglvqrro"), false) == "DDUDRLRRUDRD");
+	assert(process<17>(std::stringstream("ulqzkmiv"), false) == "DRURDRUDDLLDLUURRDULRLDUUDDDRR");
+}
